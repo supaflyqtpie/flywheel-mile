85b6fd2f8c48112b73e3f97997ca4883bbad15d7
rjiang <raymondjiang2001@yahoo.com>
2016-04-01 11:53:09 -0700

    added views and packages

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express')
+
+var exphbs = require('express-handlebars');
 var app = express()
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/static', express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.render("home");
+});
  
-app.listen(3000)
+app.listen(3000);
