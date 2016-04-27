b912381363c646a2e599f41bcb1c29628a2d88b4
Alan <alanly211@gmail.com>
2016-04-26 23:27:27 -0700

    added usertable migration, updated prod to use heroku db url

---
+++ migrations/20160427010859-createUserTable.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    queryInterface.createTable(
+      'User',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        password: {
+          Sequelize.STRING,
+          allowNull: false
+        }
+      }
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
