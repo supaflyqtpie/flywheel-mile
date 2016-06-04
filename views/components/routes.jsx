e30ef1df9f3ae86806dc50ad3ec71aef48f04770
Alan <alanly211@gmail.com>
2016-06-04 14:41:08 -0700

    setting up view for user login successful

--- views/components/routes.jsx
+++ views/components/routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import AppBase from './layout/appBase.jsx';
 import Session from './session/session.jsx';
+import Packages from './package/packages.jsx';
 import Landing from './landing.jsx';
 
 const routes = (
   <Route path="/" component={AppBase}>
     <IndexRoute component={Landing} />
-    <Route path="/login" component={Session} />
+    <Route path="login" component={Session} />
+    <Route path="packages" component={Packages} />
   </Route>
 );
 
