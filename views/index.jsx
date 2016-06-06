1d380372202a7d9c88915e794b0d99ac29fdee79
Alan <alanly211@gmail.com>
2016-06-05 18:20:44 -0700

    connected redux with react-router using react-router-redux with store

--- views/index.jsx
+++ views/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, hashHistory } from 'react-router';
-import routes from './components/routes';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store/configureStore';
+import routes from './components/routes';
 
 const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 const rootElement = document.getElementById('root');
 render(
-  <Router history={hashHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={history} routes={routes} />
+  </Provider>,
   rootElement
 );
--- views/reducers/index.jsx
+++ views/reducers/index.jsx
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 import userSessions from './session';
 
 const rootReducer = combineReducers({
   userSessions,
+  routing: routerReducer,
 });
 
 export default rootReducer;
