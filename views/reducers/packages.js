3f7629a7f029ed93d3864edae81f7dea0168d099
Alan <alanly211@gmail.com>
2016-06-25 17:00:19 -0700

    add packages ui redux

---
+++ views/reducers/packages.js
@@ -0,0 +1,79 @@
+import {
+  REQUEST_PACKAGES,
+  RECEIVED_PACKAGES,
+  ADD_PACKAGE,
+  DELETE_PACKAGE,
+  PROCESS_ADD_PACKAGE,
+  PROCESS_DELETE_PACKAGE } from '../actions/packages';
+
+function singlePackage(item, action) {
+  switch (action.type) {
+    case ADD_PACKAGE:
+      return {
+        id: action.id,
+        carrier: action.carrier,
+        isProcessingDelete: false,
+      };
+    case PROCESS_DELETE_PACKAGE:
+      if (item.id !== action.id) {
+        return item;
+      }
+      return Object.assign({}, item, {
+        isProcessingDelete: true,
+      });
+    default:
+      return item;
+  }
+}
+
+function packages(state, action) {
+  switch (action.type) {
+    case REQUEST_PACKAGES:
+      return Object.assign({}, state, {
+        isFetching: true,
+        items: [],
+      });
+    case RECEIVED_PACKAGES:
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: action.feeds,
+      });
+    case PROCESS_ADD_PACKAGE:
+      return Object.assign({}, state, {
+        isAdding: true,
+      });
+    case ADD_PACKAGE:
+      return Object.assign({}, state, {
+        isAdding: false,
+        items: [...state.items, singlePackage(undefined, action)],
+      });
+    case PROCESS_DELETE_PACKAGE:
+      return Object.assign({}, state, {
+        items: state.items.map((item) => singlePackage(item, action)),
+      });
+    case DELETE_PACKAGE:
+      return Object.assign({}, state, {
+        items: state.items.filter((item) => item.id !== action.id),
+      });
+    default:
+      return state;
+  }
+}
+
+export default function trackedPackages(state = {
+  isFetching: false,
+  isAdding: false,
+  items: [],
+}, action) {
+  switch (action.type) {
+    case REQUEST_PACKAGES:
+    case RECEIVED_PACKAGES:
+    case PROCESS_ADD_PACKAGE:
+    case PROCESS_DELETE_PACKAGE:
+    case ADD_PACKAGE:
+    case DELETE_PACKAGE:
+      return Object.assign({}, state, packages(state, action));
+    default:
+      return state;
+  }
+}
