287c65ece761be9eb774c233cb50ac6a07323480
Alan <alanly211@gmail.com>
2016-07-06 23:08:43 -0700

    set up association test and model tests for user functions, renamed package specs, turned off sequelize logging for test

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -7,24 +7,45 @@ const User = db.user;
 const email = 'dory@blue.fish';
 const password = 'findingbluefish';
 
-describe('UserModel', () => {
+describe('User Model', () => {
   beforeEach(setupDB);
 
-  describe('#User Functions', () => {
-    it('be able to create a user', () =>
-      User.createUser(email, password)
-      .then(item => {
-        expect(item.email).to.equal(email);
-        expect(item.password).to.equal(password);
-      })
-    );
+  describe('User Functions', () => {
+    describe('createUser', () => {
+      it('should be able to create a user', () =>
+        User.createUser(email, password)
+        .then(item => {
+          expect(item.email).to.equal(email);
+          expect(item.password).to.equal(password);
+        })
+      );
+    });
 
-    it('be able to find by email', () =>
-      User.create({ email, password })
-      .then(item => User.findByEmail(email)
-      .then((user) => {
-        expect(item.email).to.equal(user.email);
-        expect(item.password).to.equal(user.password);
+    describe('findByEmail', () => {
+      it('should be able to find a user by email', () =>
+        User.create({ email, password })
+        .then(item => User.findByEmail(email)
+        .then(user => {
+          expect(item.email).to.equal(user.email);
+          expect(item.password).to.equal(user.password);
+        }))
+      );
+    });
+  });
+
+  describe('has many packages', () => {
+    it('should return a user\'s packages', () =>
+      User.create({
+        email,
+        password,
+        packages: [
+          { trackingNumber: 'S0mEFunkie#', carrier: 'usps' },
+          { trackingNumber: '1800dankmemes', carrier: 'fedex' },
+        ],
+      }, {
+        include: [Package],
+      }).then(user => user.getPackages().then(packages => {
+        expect(packages.length).to.be.equal(2);
       }))
     );
   });
