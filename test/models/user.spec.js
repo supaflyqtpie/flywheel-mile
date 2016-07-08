be545548f0ebacc60c8213658f4f78a0dd334425
Alan <alanly211@gmail.com>
2016-07-08 09:33:36 -0700

    refactor user tests on brackets

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -12,29 +12,28 @@ describe('User Model', () => {
 
   describe('User Functions', () => {
     describe('createUser', () => {
-      it('should be able to create a user', () =>
+      it('should be able to create a user', () => {
         User.createUser(email, password)
         .then(item => {
           expect(item.email).to.equal(email);
           expect(item.password).to.equal(password);
-        })
-      );
+        });
+      });
     });
 
     describe('findByEmail', () => {
-      it('should be able to find a user by email', () =>
+      it('should be able to find a user by email', () => {
         User.create({ email, password })
-        .then(item => User.findByEmail(email)
-        .then(user => {
+        .then(item => User.findByEmail(email).then(user => {
           expect(item.email).to.equal(user.email);
           expect(item.password).to.equal(user.password);
-        }))
-      );
+        }));
+      });
     });
   });
 
   describe('has many packages', () => {
-    it('should return a user\'s packages', () =>
+    it('should return a user\'s packages', () => {
       User.create({
         email,
         password,
@@ -46,7 +45,7 @@ describe('User Model', () => {
         include: [Package],
       }).then(user => user.getPackages().then(packages => {
         expect(packages.length).to.be.equal(2);
-      }))
-    );
+      }));
+    });
   });
 });
