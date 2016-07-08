18555ca830d8bc6d69b9e5d71df22ca8b8975ef6
rjiang <raymondjiang2001@yahoo.com>
2016-07-08 09:28:38 -0700

    shippo api

---
+++ shippoAPIRequestHandler.js
@@ -0,0 +1,9 @@
+const baseURI = 'https://api.goshippo.com/v1/tracks';
+
+function buildURI(carrier, trackingNumber) {
+  return `${baseURI}/${carrier}/${trackingNumber}`;
+}
+
+export function shippoGet(carrier, trackingNumber) {
+  return fetch(buildURI(carrier, trackingNumber));
+}
