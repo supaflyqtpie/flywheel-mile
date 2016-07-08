4ba0c66e21f417604a99e71e8697b7446d6d2655
Alan <alanly211@gmail.com>
2016-07-08 10:27:55 -0700

    adding relation test from package to package histories

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -13,7 +13,7 @@ describe('User Model', () => {
   describe('User Functions', () => {
     describe('createUser', () => {
       it('should be able to create a user', () => {
-        User.createUser(email, password)
+        return User.createUser(email, password)
         .then(item => {
           expect(item.email).to.equal(email);
           expect(item.password).to.equal(password);
@@ -23,7 +23,7 @@ describe('User Model', () => {
 
     describe('findByEmail', () => {
       it('should be able to find a user by email', () => {
-        User.create({ email, password })
+        return User.create({ email, password })
         .then(item => User.findByEmail(email).then(user => {
           expect(item.email).to.equal(user.email);
           expect(item.password).to.equal(user.password);
@@ -34,7 +34,7 @@ describe('User Model', () => {
 
   describe('has many packages', () => {
     it('should return a user\'s packages', () => {
-      User.create({
+      return User.create({
         email,
         password,
         packages: [
