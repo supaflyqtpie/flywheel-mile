18ed4896af00ecd4c88744cf2f172676cb1302c9
Alan <alanly211@gmail.com>
2016-07-15 18:03:46 -0700

    added description in landing

--- views/components/landing/landing.jsx
+++ views/components/landing/landing.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 export default function Landing() {
   return (
     <div id="jumbotron-landing">
+      <div id="landing-title">
+        Tracking packages at the speed of dank.
+      </div>
     </div>
   );
 }
